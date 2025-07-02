08563817659b1ed7dda06bf7fb99d09d0059513e
JeniVasques <jgvvasques@gmail.com>
2025-07-02 00:00:39 -0300

    implementação do App

--- app/src/main/java/com/example/findinglogs/view/MainActivity.java
+++ app/src/main/java/com/example/findinglogs/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.findinglogs.view;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,21 +23,43 @@ public class MainActivity extends AppCompatActivity {
     private final List<Weather> weathers = new ArrayList<>();
     private FloatingActionButton fetchButton;
 
+    private boolean isRefreshingByUser = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        );
+
         setContentView(R.layout.activity_main);
         MainViewModel mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
         RecyclerView recyclerView = findViewById(R.id.recycler_view_weather);
         fetchButton = findViewById(R.id.fetchButton);
         adapter = new WeatherListAdapter(this, weathers);
         recyclerView.setAdapter(adapter);
-        mainViewModel.getWeatherList().observe(this,
-                weathers -> adapter.updateWeathers(weathers));
+        mainViewModel.getWeatherList().observe(this, weathers -> {
+            adapter.updateWeathers(weathers);
+            fetchButton.setEnabled(true);
 
+            if (isRefreshingByUser) {
+                Toast.makeText(MainActivity.this, "Atualizado", Toast.LENGTH_SHORT).show();
+                isRefreshingByUser = false;
+            }
+        });
         fetchButton.setOnClickListener(v -> {
-            mainViewModel.atualizarPagina();
+            fetchButton.setEnabled(false);
             Toast.makeText(MainActivity.this, "Atualizando...", Toast.LENGTH_SHORT).show();
+            isRefreshingByUser = true;
+            mainViewModel.atualizarPagina();
         });
     }
 }
